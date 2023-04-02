cb69cdd95847eef9ca2268c2d769a529e32bc358
dhavalkgit <dhavalk439@gmail.com>
2023-04-02 11:14:39 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("Hello this is my first jenkins job");
+		System.out.println("This code is avelable in github");
+	}
+}
+
